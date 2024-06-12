d2acce9b58f3f68e6eb3a958998d723c9afe1229
StartAt24 <ihave6questions@gmail.com>
2024-06-12 22:45:09 +0800

    coding exercise

--- BinaryTree/BinaryTree.py
+++ BinaryTree/BinaryTree.py
@@ -2,6 +2,30 @@ import sys
 
 class TreeNode:
     def __init__(val, left, right):
-        self._val = val
-        self._left = left
-        self._right = right
+        self.val = val
+        self.left = left
+        self.right = right
+    
+
+class Tree:
+    def __init__():
+        pass
+    
+    '''
+    def add(v):
+        pass
+
+    def remove(v):
+        pass
+
+    def modify(origal, target):
+        pass
+    '''
+
+    def search(TreeNode):
+        if TreeNode == None:
+            return
+
+        print(TreeNode.val)    
+        search(TreeNode.left)
+        search(TreeNode.right)
---
+++ BinaryTree/Set.py
@@ -0,0 +1,9 @@
+class MySet:
+    c = []
+    def AddElement(a):
+        if(c.contains(a)):
+            return
+        else:
+            c.append(a)
+
+add remove modify search
